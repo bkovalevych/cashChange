7357702d5b628b7489064a526ce108867af17bb1
bkovalevych <32486313+bkovalevych@users.noreply.github.com>
2018-04-03 12:41:32 +0300

    Add files via upload

---
+++ ApplicationViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace CashChange
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private City selectedCity;
+
+        public ObservableCollection<City> ArrayCity { get; set; }
+        
+        private RelayCommand addCity;
+        public RelayCommand AddCity
+        {
+            get
+            {
+                return addCity ??
+                  (addCity = new RelayCommand(obj =>
+                  {
+                      City city = new City("", 0, 0);
+                      ArrayCity.Insert(0, city);
+                      SelectedCity = city;
+                  }));
+            }
+        }
+
+        private RelayCommand removeCity;
+        public RelayCommand RemoveCity
+        {
+            get
+            {
+                return removeCity ??
+                (removeCity = new RelayCommand(obj =>
+                {
+                    City city = obj as City;
+                    if (city != null)
+                    {
+                        ArrayCity.Remove(city);
+                    }
+                },
+                (obj) => ArrayCity.Count > 0));
+            }
+        }
+
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            {
+                selectedCity = value;
+                OnPropertyChanged("SelectedCity");
+            }
+        }
+
+        public ApplicationViewModel()
+        {
+            ArrayCity = new ObservableCollection<City>(City.GetCity());
+           
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ City.cs
@@ -0,0 +1,71 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace CashChange
+{
+    public class City : INotifyPropertyChanged
+    {
+        private string name;
+        private double xCoords;
+        private double yCoords;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+   
+            }
+        }
+        public double XCoords
+        {
+            get { return xCoords; }
+            set
+            {
+                xCoords = value;
+                OnPropertyChanged("XCoords");
+            }
+        }
+        public double YCoords
+        {
+            get { return yCoords; }
+            set
+            {
+                yCoords = value;
+                OnPropertyChanged("YCoords");
+            }
+        }
+        public City(string name, double x, double y)
+        {
+            Name = name;
+            XCoords = x;
+            YCoords = y;
+        }
+        static public City[] GetCity()
+        {
+            TextReader reader = null;
+            try
+            {
+                reader = new StreamReader(@"C:\Users\Bohdan\source\repos\CashChange\CashChange\City.txt");
+                string text = reader.ReadToEnd();
+                return JsonConvert.DeserializeObject<City[]>(text);
+            }
+            finally
+            {
+                reader.Close();
+            }
+            
+           
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
